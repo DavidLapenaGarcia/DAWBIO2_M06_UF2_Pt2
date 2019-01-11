1396b517ef333fdeb9799583a9c064e7d02b246d
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-11 16:58:27 +0100

    f4 fixed, it works

--- js/control/f4.js
+++ js/control/f4.js
@@ -1,12 +1,12 @@
 
 /*
-@name= (document).ready()
-@author= David Lapena Garcia
-@version= 1.0
+@name        = (document).ready()
+@author      = David Lapena Garcia
+@version     = 1.0
 @description = Starts the app. First, shows the correct frame and hides the others
 Then, makes the logo dance.
-@date = 18-12-2018
-@params= none
+@date   = 18-12-2018
+@params = none
 @return = none
 */
 $(document).ready(function (event){
@@ -16,17 +16,18 @@ $(document).ready(function (event){
         event.preventDefault(); 
         var x = $("#x").val();
         var y = $("#y").val();
-
-        var heigth =$("#heigth").val();
-        var width =$("#weigth").val(); 
-
-        var body= "width="+width+",height="+heigth;
-        alert(heigth); 
         
-        var windo = window.open("./../../pop-Up/f4Pop.html", "_blank", body); 
-        windo.screenX = x;
-        windo.screenY = y;
+        var heigth = $("#heigth").val();
+        var width  = $("#width").val();
+        
+        var body = "'width="+width+",height="+heigth+"'";
+        alert(body); 
+        
+        var windo         = window.open("./../../pop-Up/f4Pop.html", "_blank", "width=400,height=400");
+        var windo         = window.open("./../../pop-Up/f4Pop.html", "_blank", body);
+            windo.screenX = x;
+            windo.screenY = y;
         windo.width(width);
-        //IDONT KNOW DO THIS SHITTT
+        
   })
 });
